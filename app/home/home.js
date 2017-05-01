d065e878f88472e09cfbb26013d80155ecfd5a23
pawel suminski <pawel.suminski@fingoweb.com>
2017-05-01 17:14:16 +0200

    asdasd

--- app/home/home.js
+++ app/home/home.js
@@ -14,8 +14,12 @@ HomeController.$inject = ['Event'];
 
 function HomeController(Event) {
     var vm = this;
-
-    Event.getAll().then(getSuccess, getFailed);
+    vm.Events = [
+        {'name': 'Event 1', description: 'Opis eventu 1'},
+        {'name': 'Event 2', description: 'Opis eventu 2'},
+        {'name': 'Event 3', description: 'Opis eventu 3'}
+    ];
+    //Event.getAll().then(getSuccess, getFailed);
 
     function getSuccess(events) {
         vm.Events = events;
